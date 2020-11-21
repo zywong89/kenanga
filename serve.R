43ae9841b5b6bd45aa5b3095287cd524a1dd582d
zywong89 <zywong1989@yahoo.com.my>
2020-11-21 17:28:45 +0800

    Removed irrelevant lines

--- serve.R
+++ serve.R
@@ -1,10 +1,6 @@
 # Source Directory
 setwd("./path/to/this/script")
-setwd("~/../Desktop/Job Applications/Kenanga/Data Scientist Assignment")
 
 library(plumber)
 r <- plumb("api.R")
 r$run(port = 8080)
-
-
-# Example to call: http://127.0.0.1:8080/predict?refit=F
